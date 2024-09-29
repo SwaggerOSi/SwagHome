88324fb064f33e2103559a447cf3d99c1ef62a63
Swagger <vladimiryaroslavcev3@gmail.com>
2024-09-29 20:08:58 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Name:Vladimir"
+print(name, type(name))
+age = "Age:31"
+print(age, type(age))
+new_age = "New Age:32"
+print(new_age, type(new_age))
+is_student = "Is Student: True"
+print(is_student, type(is_student))
