350851d3154257744f3f2db0d50bbe84e074e856
Swagger <vladimiryaroslavcev3@gmail.com>
2024-09-29 21:06:11 +0300

    Add files via upload

---
+++ main-test.py
@@ -0,0 +1,8 @@
+home_works = 12 #выполненныъх заданий
+#печать(home_works, ввести(home_works))
+time_works = 1.5 #затраченных часов
+#печать (time_works, введите(time_works))
+name_curs = 'Python' #название курса
+time_one_les = (time_works/home_works)
+
+print('Курс: ', name_curs, ", всего задач:", home_works, ", затрачено часов:" , time_works, ", среднее время выполнения", time_one_les)
